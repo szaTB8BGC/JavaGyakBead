8268f35d5ef63d0b967239281b67b901acded9db
szabi9250 <202984975+szabi9250@users.noreply.github.com>
2025-11-22 11:28:13 +0100

    Regisztrálás elkészítése

--- src/main/java/com/example/gyakorlatbead/HomeController.java
+++ src/main/java/com/example/gyakorlatbead/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.gyakorlatbead;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -16,5 +21,38 @@ public class HomeController {
     @GetMapping("/admin/home")
     public String admin() {
         return "admin";
+    }
+    @GetMapping("/jelszoteszt")
+
+    @ResponseBody
+    public String jelszoTeszt() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder.encode("jelszo1");
+    }
+
+    @GetMapping("/regisztral")
+    public String greetingForm(Model model) {
+        model.addAttribute("reg", new User());
+        return "regisztral";
+    }
+
+    @Autowired
+    private UserRepository userRepo;
+    @PostMapping("/regisztral_feldolgoz")
+    public String regisztracio(@ModelAttribute User user, Model model) {
+        // Ellenőrizzük, hogy van-e már felhasználó az adott email címmel
+        for (User felhasznalo2 : userRepo.findAll()) {
+            if (felhasznalo2.getEmail().equals(user.getEmail())) {
+                model.addAttribute("uzenet", "A regisztrációs email már foglalt!");
+                return "reghiba";
+            }
+        }
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_Vendeg");
+        userRepo.save(user);
+        model.addAttribute("id", user.getId());
+        return "regjo";
     }
 }
---
+++ src/main/java/com/example/gyakorlatbead/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.gyakorlatbead;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+public class WebSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers ("/resources/**", "/", "/regisztral", "/regisztral_feldolgoz").anonymous()
+                        .requestMatchers("/","/jelszoteszt").anonymous()
+                        .requestMatchers("/resources/**", "/", "/home").authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/home")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
